be5a6063d3767674fb03dece06abf5eb674fb8c1
Albert Goncalves <goncalves.albert@gmail.com>
2020-07-26 19:30:29 -0400

    move scattered parameters to global scope

--- src/main.js
+++ src/main.js
@@ -2,14 +2,97 @@
 
 "use strict";
 
-var TAU = Math.PI * 2.0;
+var VIEWPORT = {
+    offset: {
+        x: 0.0,
+        y: 0.0,
+    },
+    width: undefined,
+    height: undefined,
+};
+var WORLD_SPACE = {
+    center: {
+        x: 0.0,
+        y: 0.0,
+    },
+    width: 10.0,
+    height: undefined,
+    halfWidth: undefined,
+    halfHeight: undefined,
+    cameraDistance: 1.0,
+};
+var VERTICES = new Float32Array([
+    1.0,
+    1.0,
+    -1.0,
+    1.0,
+    1.0,
+    -1.0,
+    -1.0,
+    -1.0,
+]);
+var BACKGROUND_COLOR = {
+    red: 0.85,
+    green: 0.85,
+    blue: 0.85,
+    alpha: 1.0,
+};
+var SPEED_MOVE = 0.085;
+var SPEED_ROTATE = 0.005;
+var FPS = 60.0;
+var STATE = {
+    alive: true,
+    currentTime: 0.0,
+    previousTime: 0.0,
+    elapsedTime: 0.0,
+    lagTime: 0.0,
+    msPerFrame: 1000.0 / FPS,
+    keyDown: new Array(MAX_KEY_CODE),
+    objects: [
+        {
+            rect: {
+                x: 0.0,
+                y: 0.0,
+                width: 1.0,
+                height: 1.0,
+                rotate: (Math.PI * 2.0) - 0.785,
+            },
+            color: new Float32Array([0.25, 0.65, 0.875, 1.0])
+        },
+        {
+            rect: {
+                x: 0.0,
+                y: 0.0,
+                width: 2.5,
+                height: 2.5,
+                rotate: 0.2,
+            },
+            color: new Float32Array([0.25, 0.875, 0.65, 1.0])
+        },
+    ],
+    transform: mat4.create(),
+};
 var KEY_CODE = {
     left: 37,
     up: 38,
     right: 39,
     down: 40,
+    i: 73,
+    j: 74,
+    k: 75,
+    l: 76,
 };
 var MAX_KEY_CODE = 256;
+
+function setGlobals(canvas) {
+    VIEWPORT.width = canvas.width - (VIEWPORT.offset.x * 2.0);
+    VIEWPORT.height = canvas.height - (VIEWPORT.offset.y * 2.0);
+    WORLD_SPACE.height =
+        WORLD_SPACE.width / (VIEWPORT.width / VIEWPORT.height);
+    WORLD_SPACE.halfWidth = WORLD_SPACE.width / 2.0;
+    WORLD_SPACE.halfHeight = WORLD_SPACE.height / 2.0;
+    STATE.keyDown.fill(false);
+}
 
 function getCompiledShader(gl, id, type) {
     var shader = gl.createShader(type);
@@ -23,22 +106,8 @@ function getCompiledShader(gl, id, type) {
 }
 
 function setVertexBuffer(gl) {
-    var vertices = new Float32Array([
-        1.0,
-        1.0,
-        0.0,
-        -1.0,
-        1.0,
-        0.0,
-        1.0,
-        -1.0,
-        0.0,
-        -1.0,
-        -1.0,
-        0.0,
-    ]);
     gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, VERTICES, gl.STATIC_DRAW);
 }
 
 function getShaders(gl) {
@@ -57,7 +126,7 @@ function getShaders(gl) {
         console.error(gl.getProgramInfoLog(program));
     }
     var positionAttribute = gl.getAttribLocation(program, "position");
-    gl.vertexAttribPointer(positionAttribute, 3, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(positionAttribute, 2, gl.FLOAT, false, 0, 0);
     return {
         positionAttribute: positionAttribute,
         program: program,
@@ -65,167 +134,112 @@ function getShaders(gl) {
     };
 }
 
-function setGl(canvas, gl, shaders, color) {
-    var viewport = {
-        offset: {
-            x: 20.0,
-            y: 20.0,
-        },
-    };
-    viewport.width = canvas.width - (viewport.offset.x * 2.0);
-    viewport.height = canvas.height - (viewport.offset.y * 2.0);
-    var worldSpace = {
-        width: 10.0,
-        center: {
-            x: 0.0,
-            y: 0.0,
-        },
-        cameraDistance: 1.0,
-    };
-    worldSpace.height = worldSpace.width / (viewport.width / viewport.height);
-    worldSpace.halfWidth = worldSpace.width / 2.0;
-    worldSpace.halfHeight = worldSpace.height / 2.0;
+function setGl(canvas, gl, shaders) {
     var lookAt = mat4.create();
-    mat4.lookAt(
-        lookAt,
-        [worldSpace.center.x, worldSpace.center.y, worldSpace.cameraDistance],
-        [worldSpace.center.x, worldSpace.center.y, 0.0],
-        [0.0, 1.0, 0.0]);
+    mat4.lookAt(lookAt,
+                [
+                    WORLD_SPACE.center.x,
+                    WORLD_SPACE.center.y,
+                    WORLD_SPACE.cameraDistance
+                ],
+                [WORLD_SPACE.center.x, WORLD_SPACE.center.y, 0.0],
+                [0.0, 1.0, 0.0]);
     var ortho = mat4.create();
     mat4.ortho(ortho,
-               -worldSpace.halfWidth,
-               worldSpace.halfWidth,
-               -worldSpace.halfHeight,
-               worldSpace.halfHeight,
+               -WORLD_SPACE.halfWidth,
+               WORLD_SPACE.halfWidth,
+               -WORLD_SPACE.halfHeight,
+               WORLD_SPACE.halfHeight,
                0.0,
-               worldSpace.cameraDistance);
+               WORLD_SPACE.cameraDistance);
     var projection = mat4.create();
     mat4.multiply(projection, ortho, lookAt);
-    gl.viewport(viewport.offset.x,
-                viewport.offset.y,
-                viewport.width,
-                viewport.height);
-    gl.clearColor(color.red, color.green, color.blue, color.alpha);
+    gl.viewport(VIEWPORT.offset.x,
+                VIEWPORT.offset.y,
+                VIEWPORT.width,
+                VIEWPORT.height);
+    gl.clearColor(BACKGROUND_COLOR.red,
+                  BACKGROUND_COLOR.green,
+                  BACKGROUND_COLOR.blue,
+                  BACKGROUND_COLOR.alpha);
     gl.useProgram(shaders.program);
     gl.uniformMatrix4fv(shaders.uniform.projection, false, projection);
     gl.enableVertexAttribArray(shaders.positionAttribute);
 }
 
-function getColorArray(red, green, blue, alpha) {
-    return new Float32Array([red, green, blue, alpha]);
-}
-
-function getTransform(rect) {
-    var transform = mat4.create();
-    mat4.translate(transform, transform, vec3.fromValues(rect.x, rect.y, 0.0));
-    mat4.scale(transform,
-               transform,
+function setTransform(rect) {
+    mat4.identity(STATE.transform);
+    mat4.translate(STATE.transform,
+                   STATE.transform,
+                   vec3.fromValues(rect.x, rect.y, 0.0));
+    mat4.scale(STATE.transform,
+               STATE.transform,
                vec3.fromValues(rect.width, rect.height, 1.0));
-    mat4.rotateZ(transform, transform, rect.rotate);
-    return transform;
+    mat4.rotateZ(STATE.transform, STATE.transform, rect.rotate);
 }
 
-function update(state, objects) {
-    for (var i = objects.length - 1; 0 <= i; --i) {
-        objects[i].rect.rotate += 0.005 * (i + 1);
-        if (state.keyDown[KEY_CODE.up]) {
-            objects[i].rect.y += 0.05;
+function update() {
+    for (var i = STATE.objects.length - 1; 0 <= i; --i) {
+        STATE.objects[i].rect.rotate += SPEED_ROTATE * (i + 1);
+        if (STATE.keyDown[KEY_CODE.up] || STATE.keyDown[KEY_CODE.i]) {
+            STATE.objects[i].rect.y += SPEED_MOVE;
         }
-        if (state.keyDown[KEY_CODE.down]) {
-            objects[i].rect.y -= 0.05;
+        if (STATE.keyDown[KEY_CODE.down] || STATE.keyDown[KEY_CODE.k]) {
+            STATE.objects[i].rect.y -= SPEED_MOVE;
         }
-        if (state.keyDown[KEY_CODE.left]) {
-            objects[i].rect.x -= 0.05;
+        if (STATE.keyDown[KEY_CODE.left] || STATE.keyDown[KEY_CODE.j]) {
+            STATE.objects[i].rect.x -= SPEED_MOVE;
         }
-        if (state.keyDown[KEY_CODE.right]) {
-            objects[i].rect.x += 0.05;
+        if (STATE.keyDown[KEY_CODE.right] || STATE.keyDown[KEY_CODE.l]) {
+            STATE.objects[i].rect.x += SPEED_MOVE;
         }
     }
 }
 
-function draw(gl, shaders, objects) {
+function draw(gl, shaders) {
     gl.clear(gl.COLOR_BUFFER_BIT);
-    for (var i = objects.length - 1; 0 <= i; --i) {
-        gl.uniform4fv(shaders.uniform.color, objects[i].color);
-        gl.uniformMatrix4fv(shaders.uniform.transform,
-                            false,
-                            getTransform(objects[i].rect));
+    for (var i = STATE.objects.length - 1; 0 <= i; --i) {
+        setTransform(STATE.objects[i].rect);
+        gl.uniformMatrix4fv(shaders.uniform.transform, false, STATE.transform);
+        gl.uniform4fv(shaders.uniform.color, STATE.objects[i].color);
         gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
     }
 }
 
-function keyDown(state) {
-    return function(event) {
-        state.keyDown[event.keyCode] = true;
-    };
+function keyDown(event) {
+    STATE.keyDown[event.keyCode] = true;
 }
 
-function keyUp(state) {
-    return function(event) {
-        state.keyDown[event.keyCode] = false;
+function keyUp(event) {
+    STATE.keyDown[event.keyCode] = false;
+}
+
+function loop(gl, shaders) {
+    return function(t) {
+        if (STATE.alive) {
+            STATE.currentTime = t;
+            STATE.elapsedTime = STATE.currentTime - STATE.previousTime;
+            STATE.previousTime = STATE.currentTime;
+            STATE.lagTime += STATE.elapsedTime;
+            while (STATE.alive && (STATE.msPerFrame <= STATE.lagTime)) {
+                update();
+                STATE.lagTime -= STATE.msPerFrame;
+            }
+            draw(gl, shaders);
+            requestAnimationFrame(loop(gl, shaders));
+        }
     };
 }
 
 window.onload = function() {
     var canvas = document.getElementById("canvas");
+    setGlobals(canvas);
     var gl = canvas.getContext("webgl");
     setVertexBuffer(gl);
     var shaders = getShaders(gl);
-    setGl(canvas, gl, shaders, {
-        red: 0.85,
-        green: 0.85,
-        blue: 0.85,
-        alpha: 1.0,
-    });
-    var state = {
-        fps: 60.0,
-        alive: true,
-        currentTime: Date.now(),
-        previousTime: Date.now(),
-        elapsedTime: 0.0,
-        lagTime: 0.0,
-        keyDown: new Array(MAX_KEY_CODE),
-    };
-    state.keyDown.fill(false);
-    state.msPerFrame = 1000.0 / state.fps;
-    window.addEventListener("keydown", keyDown(state));
-    window.addEventListener("keyup", keyUp(state));
-    var objects = [
-        {
-            rect: {
-                x: 0.0,
-                y: 0.0,
-                width: 1.0,
-                height: 1.0,
-                rotate: TAU - 0.785,
-            },
-            color: getColorArray(0.25, 0.65, 0.875, 1.0),
-        },
-        {
-            rect: {
-                x: 0.0,
-                y: 0.0,
-                width: 2.5,
-                height: 2.5,
-                rotate: 0.2,
-            },
-            color: getColorArray(0.25, 0.875, 0.65, 1.0),
-        },
-    ];
-    function loop() {
-        if (state.alive) {
-            state.currentTime = Date.now();
-            state.elapsedTime = state.currentTime - state.previousTime;
-            state.previousTime = state.currentTime;
-            state.lagTime += state.elapsedTime;
-            while (state.alive && (state.msPerFrame <= state.lagTime)) {
-                update(state, objects);
-                state.lagTime -= state.msPerFrame;
-            }
-            draw(gl, shaders, objects);
-            requestAnimationFrame(loop);
-        }
-    }
-    loop();
+    setGl(canvas, gl, shaders);
+    window.addEventListener("keydown", keyDown);
+    window.addEventListener("keyup", keyUp);
+    requestAnimationFrame(loop(gl, shaders));
     console.log("Done!");
 };
